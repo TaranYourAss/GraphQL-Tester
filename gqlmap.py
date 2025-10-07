eaa4d173f611afd49e0fce759f4f1ead8850f3f0
Taran Ulrich <65303833+TaranYourAss@users.noreply.github.com>
2025-10-07 11:40:48 -0600

    10/07#01
    - implmented alias & directive overload testing
    - implemented performance plotting of overloads

--- gqlmap.py
+++ gqlmap.py
@@ -11,6 +11,7 @@ try:
     import threading
     import logging
     import traceback
+    import plotext as plt
 
     import lib.parse.cmdargs as cmdargs
     from lib.core.init import conf
@@ -20,6 +21,8 @@ try:
     from lib.core.common import print_banner
     from lib.core.common import stdoutWrite
     from lib.core.graphql import GraphQL
+    from lib.vuln_testing.overload_dos import directive_overloading
+    from lib.vuln_testing.overload_dos import alias_overloading
 
 
       
@@ -37,7 +40,7 @@ except KeyboardInterrupt:
 #   - if introspection enabled, check if description fields are available - potentially has sensitive info 
 # - test against graphql-armor
 # - threading?
-# - finish loading cursor
+# - finish loadingCursor method
 # - add option to not verify SSL certificates
 # - add option to set timeout
 # - add option to set user-agent
@@ -61,88 +64,7 @@ except KeyboardInterrupt:
 #   - Authorization Bypass via Introspection - COPILOT GENEREATED THIS - IDK WHAT THIS IS
 #   - GraphQL Query Depth Analysis - COPILOT GENEREATED THIS - IDK WHAT THIS IS
 
-"""
-def directive_overloading(url, headers=None):
-    
-    Returns performance data which contains two lists: [response_time_milliseconds, overload_count]
-    
-    query = "query overload {__typename @include(if:true) @include(if:true) @include(if:true)}"
-    performance_data = [[], []] # [response_time_milliseconds, overload_count]
-    #response_tracking = []
-    
-    #make sure it is a valid url and works
-    results = make_post_request(
-        url=url,
-        json_data={"query": query},
-        headers=headers
-    )
 
-    #TODO: add something to detect if cloudflare is blocking us
-    # Cloudflare</title>
-    # <h1 data-translate="block_headline">Sorry, you have been blocked</h1>
-    # 
-    print(f"Initial test of {url}")
-    print(f"Status Code: {results['status_code']}, Response Time (ms): {results['response_time_milliseconds']}, Query: {query}")
-    
-    if results['status_code'] == 403:
-        print(results['content'])
-        exit()
-
-    if results['json'] is None:
-        print("Response is not JSON, something went wrong")
-        print(results['content'])
-        return
-    #might need to get rid of this to handle other GraphQL engines
-    if 'data' not in results['json']:
-        print("No data field in response, something went wrong")
-        print(results['content'])
-        return
-    
-    #baseline_response_time = results['response_time_milliseconds']
-    #x = 0
-    overload_count = 3
-    while True: #loop until we hit a stopping condition
-        overload_count = overload_count * 2
-        query = f"query overload {{__typename {'@include(if:true) ' * overload_count}}}"
-        results = make_post_request(
-            url=url,
-            json_data={"query": query},
-            headers=headers
-        )
-        if results['status_code'] == 403:
-            print(results['content'])
-            exit()
-        #if not 'data' in results['json']:
-        #    print("No data field in response, something went wrong")
-        #    print(results['content'])
-        #    return
-        #check if we got a non-200 status code, cloudflare may timeout or block us
-        if results['status_code'] != 200:
-            print(f"Non-200 status code received, stopping test. Status Code: {results['status_code']} - Response Time (ms): {results['response_time_milliseconds']}")
-            if "y" or "yes" in input("Do you want to see the full response? (y/n): ").lower():
-                print(results['content'])
-            return performance_data
-        
-        performance_data[1].append(overload_count)
-        performance_data[0].append(results['response_time_milliseconds'])
-        
-
-        print(f"Overload Count: {overload_count} - Response Time (ms): {results['response_time_milliseconds']}")
-
-        if results['response_time_milliseconds'] > 60000: #if the response time is over 60 seconds stop the test
-            print("Response time exceeded 60 seconds, stopping test")
-            break
-        
-        # TODO: add a way to check if the response time is constnantly increasing
-        # need to make sure that the repsonse times are tied to the overload count
-        # - if the overload count is increasing but the response time is not the application is not vulnerable
-        #    - could be using timouts to kill processing of the query
-        #    - could have a limit to directives
-        #    - could be filtering directives with a WAF
-        #x += 1
-        time.sleep(1) #be nice to the server
-    return performance_data
-    """
 def main():
     """
     Main function of gqlmap.
@@ -155,8 +77,15 @@ def main():
 
         stdoutWrite(f"[*] Started at %s\n\n" % time.strftime("%X %Y/%m/%d"))
 
-        TARGET = GraphQL()
 
+        TARGET = GraphQL()
+        #TODO integrate with GraphQL class
+        alias_overloading_data = alias_overloading(url=TARGET.url, headers=TARGET.headers, max_overloads=100000)
+        plt.simple_bar(alias_overloading_data[1], alias_overloading_data[0], title="Alias Overloading - Overload Count vs Response Time (ms)", width=100)
+        plt.show()
+        directive_overloading_data = directive_overloading(url=TARGET.url, headers=TARGET.headers, max_overloads=100000)
+        plt.simple_bar(directive_overloading_data[1], directive_overloading_data[0], title="Directive Overloading - Overload Count vs Response Time (ms)", width=100)
+        plt.show()
     except Exception as errMsg:
         excMsg = traceback.format_exc()
         logger.critical("%s\n%s" % (errMsg, excMsg))
